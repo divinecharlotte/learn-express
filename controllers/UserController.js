51e02afc9c66c853762022f269924eb1869727cb
divine <divinemaina@gmail.com>
2023-02-04 17:32:41 +0200

    add user authentication

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,27 +1,6 @@
 const Joi = require('joi');
 const User = require("../models/User");
 
-let user_creation =  async (req,res) => {
-		
-    const validateUser = (data) => {
-        const schema = Joi.object({
-            email: Joi.string().email().required(),
-            password: Joi.string().regex(/^[a-z]+$/).required()
-        });
-        return schema.validate(data);
-        }
-	
-    const { error, value } = validateUser(req.body);
-    if (error) return res.status(400).send(error.message);
-    
-    const user = new User({
-        email: value.email,
-        password: value.password,
-    });
-    console.log(req.body.password);
-    await user.save();
-    res.send(user);
-}
 
 let get_user = async (req, res) => {
 		
@@ -34,6 +13,5 @@ let get_user = async (req, res) => {
     }
    }
 module.exports = {
-    user_creation,
     get_user
 }
--- routes.js
+++ routes.js
@@ -21,8 +21,6 @@ cloudinary.config({
   const upload = multer({ storage: storage });
 
 
-
-router.post("/users", UserController.user_creation)
 router.get("/users", UserController.get_user)
 router.post("/blogs", upload.single("image"),BlogController.blog_creation)
 router.get("/blogs/:id",BlogController.get_blog)
