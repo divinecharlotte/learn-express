3ccb8ec81465985f9e13d7c9458661287bb1e2d9
divine <divinemaina@gmail.com>
2023-02-06 03:07:00 +0200

    update blog model

--- models/Blog.js
+++ models/Blog.js
@@ -8,16 +8,7 @@ const BlogSchema = new schema({
 })
 
 const Blog = mongoose.model("Blog",BlogSchema)
-// export default mongoose.model("Blog", schema)
+
 export default Blog
 
 
-// const mongoose = require("mongoose")
-
-// const schema = mongoose.Schema({
-// 	title: String,
-// 	content: String,
-// 	image: String,
-// })
-
-// module.exports = mongoose.model("Blog", schema)
