4a3a0412eca086aa869514d38a625f7a8365cef8
divine charlotte <divinemaina@gmail.com>
2023-02-21 10:52:56 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import express from "express";
 
 import morgan from "morgan";
-
+import cors from "cors"
 import routes from "./routes.js";
 import sessions from "express-session";
 const app = express();
-
+app.use(cors());
 app.use(morgan("combined"));
 app.use(express.json());
 app.use(sessions({
